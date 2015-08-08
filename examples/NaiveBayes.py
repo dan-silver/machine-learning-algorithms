be6b481eddba92f7056ade2542d9070a573dfd61
Dan Silver <dannysilver3@gmail.com>
2015-08-07 21:22:11 -0700

    more nb

--- examples/NaiveBayes.py
+++ examples/NaiveBayes.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import sys
 sys.path.append("..")
 from naive_bayes import NaiveBayes
@@ -6,19 +7,16 @@ import pandas as pd
 
 data = loadData('iris.data')
 
-trainSet, testSet = splitData(data, 0.66)
+data = pd.DataFrame({
+	'a':[1,2,3],
+	'b':[20,21,22],
+	'class':[1,0,0]
+})
+# print data
+trainSet, testSet = splitData(data, 1)
 
 trainY, trainX = extractColumn(trainSet, 'class')
 testY, testX = extractColumn(testSet, 'class')
 
 cf = NaiveBayes()
 cf.fit(trainX, trainY)
-
-print cf.splitDataByResult()
-
-# predictions = nn.predict(testX)
-
-# print predictions
-
-# accuracy = getAccuracy(testY, predictions)
-# print('Accuracy: ' + repr(accuracy) + '%')
